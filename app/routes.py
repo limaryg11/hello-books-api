ff0e608f58dd1a5433ed1bbc77be6ac28199e5b9
Limary Gonzalez <limaryg11@gmail.com>
2023-04-19 19:23:49 -0700

    refactored helper function to add abort and make_response to be able to handle invalid input

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,4 @@
-
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -16,6 +15,23 @@ books = [
 hello_world_bp = Blueprint("hello_world", __name__)
 books_bp = Blueprint("books", __name__, url_prefix = "/books")
 
+def validate_book(book_id):
+
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({
+        "message": f"book {book_id} invalid"
+        }, 400))
+    
+    for book in books:
+        if book.id == book_id:
+            return book
+    
+    abort(make_response({
+        "message": f"book {book_id} not found"
+        }, 404))
+
 @books_bp.route("", methods = ["GET"])
 def handle_books():
     books_response =[]
@@ -30,21 +46,10 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        return {
-        "message": f"book {book_id} invalid"
-        }, 400
+    book = validate_book(book_id)
 
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book_id,
-                "title": book.title,
-                "description": book.description
-            }
-        
     return {
-        "message": f"book {book_id} not found"
-        }, 404
+        "id": book_id,
+        "title": book.title,
+        "description": book.description
+    }
